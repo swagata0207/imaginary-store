e9b927ee49cffa9386b1b83135d69cd772820b21
swagata0207 <its.swagata.sasmal@gmail.com>
2024-08-30 03:34:17 +0530

    added class details

--- imst-libraries/schema/src/main/java/dto/CustomerDTO.java
+++ imst-libraries/schema/src/main/java/dto/CustomerDTO.java
@@ -1,4 +1,38 @@
 package dto;
 
+import customclases.Address;
+import entity.Customer;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CustomerDTO {
+    private String customerId;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhoneNo;
+    private Address customerAddress;
+    public static Customer prepareCustomerEntity(CustomerDTO customerDTO) {
+
+        return new Customer(
+                customerDTO.getCustomerId(),
+                customerDTO.getCustomerName(),
+                customerDTO.getCustomerEmail(),
+                customerDTO.getCustomerPhoneNo(),
+                customerDTO.getCustomerAddress());
+    }
+
+    public static CustomerDTO prepareCustomerDTO(Customer customer) {
+        return new CustomerDTO(
+                customer.getCustomerId(),
+                customer.getCustomerName(),
+                customer.getCustomerEmail(),
+                customer.getCustomerPhoneNo(),
+                customer.getCustomerAddress()
+        );
+    }
 }
--- imst-libraries/schema/src/main/java/entity/Customer.java
+++ imst-libraries/schema/src/main/java/entity/Customer.java
@@ -1,5 +1,20 @@
 package entity;
 
+import customclases.Address;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Customer {
+    private String customerId;
+    private String customerName;
+    private String customerEmail;
+    private String customerPhoneNo;
+    private Address customerAddress;
 }
