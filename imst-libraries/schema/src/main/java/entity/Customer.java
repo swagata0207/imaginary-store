cc3a8b0fc4476dd1217de982278387ea398bbb49
swagata0207 <swagata.94@outlook.com>
2024-11-27 01:23:10 +0530

    updated customer schema

--- imst-libraries/schema/src/main/java/entity/Customer.java
+++ imst-libraries/schema/src/main/java/entity/Customer.java
@@ -1,17 +1,21 @@
 package entity;
 
 import customclases.Address;
-import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import org.springframework.data.mongodb.core.mapping.Document;
+
 @Getter @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Document
 public class Customer {
+
+    @Id
     private String customerId;
     private String customerName;
     private String customerEmail;
